a7c49b6dcf6ff0f77be7111bcd897468576cc22c
gonzodeveloper <doctor.kyle.hart@gmail.com>
2025-05-25 20:48:17 -1000

    api updates

--- tarxiv/api.py
+++ tarxiv/api.py
@@ -28,25 +28,18 @@ class API(TarxivModule):
         }
 
         # Now build a dictionary of valid values
-        self.valid_fields = {
-            "filter": self.txv_db.valid_filters(),
-            "object_type": self.txv_db.valid_object_types(),
-            "reporting_group": self.txv_db.valid_groups(),
-
-        }
-        self.valid_operators = ['<', '>', '=', '<=', '>=', "IN"]
-
+        self.valid_operators = ['<', '>', '=', '<=', '>=', 'IN', 'LIKE']
 
         # Build application
         self.app = Flask(__name__)
         self.app.register_blueprint(Blueprint('main', __name__))
 
+
+    def start_server(self):
         # Enable WSGI access logging via Paste
         app_logged = TransLogger(self.app)
-
         # Mount the WSGI callable object (app) on the root directory
         cherrypy.tree.graft(app_logged, '/')
-
         # Set the configuration of the web server
         cherrypy.config.update({
             'engine.autoreload.on': True,
@@ -54,7 +47,6 @@ class API(TarxivModule):
             'server.socket_port': self.config['api_port'],
             'server.socket_host': '0.0.0.0'
         })
-
         # Start the CherryPy WSGI web server
         cherrypy.engine.start()
         cherrypy.engine.block()
@@ -93,7 +85,6 @@ class API(TarxivModule):
 
         @self.app.route('/get_object_lc/<string:obj_name>', methods=['POST'])
         def get_object_lc(obj_name):
-
             # Get request json
             request_json = request.get_json()
             token = request_json['token']
@@ -133,12 +124,16 @@ class API(TarxivModule):
                 query_str = ("SELECT * "
                              "FROM the tarxiv._default.tns_objects "
                              "WHERE 1=1 ")
-
                 # Add restrictions from search fields, then append search params to query
                 condition_list = []
-                for field in request_json['search_fields']:
-                    pass
-
+                for condition in request_json['search_fields']:
+                    condition_str = self.build_condition(condition)
+                    condition_list.append(condition_str)
+                # Append full condition string to query string
+                full_condition_string = " AND ".join(condition_list)
+                query_str += full_condition_string
+                # Return results
+                result = self.txv_db.query(query_str)
 
             except PermissionError as e:
                 result = {"error": str(e)}
@@ -149,6 +144,8 @@ class API(TarxivModule):
             except Exception as e:
                 result = {"error": str(e), "type": "server"}
                 status_code = 500
+            finally:
+                return server_response(result, status_code)
 
     def build_condition(self, query_json):
         field_name = query_json["field"]
@@ -171,10 +168,30 @@ class API(TarxivModule):
 
         return condition_str + "AND ".join(predicates) + "END "
 
-    def build_predicate(self, field_name, operator, operator_value, value):
-
-        return ""
-
+    def build_predicate(self, field_name, search_field, operator, search_value):
+        # Check valid operators
+        if operator not in self.valid_operators:
+            raise ValueError(f"bad operator {operator}")
+        # Simple check against SQL injection, allow max two words in query string, semicolons or comment str
+        if type(search_value) == str:
+            if any(char in search_value for char in [";", "/*", "*/", "--"]):
+                raise ValueError(f"search field contains invalid characters {search_value}")
+        if type(search_value) == list:
+            for item in search_value:
+                if any(char in search_value for char in [";", "/*", "*/", "--"]):
+                    raise ValueError(f"search field contains invalid characters {search_value}")
+        # Format predicate search value
+        if type(search_value) in [int, float]:
+            value_str = str(search_value)
+        elif type(search_value) == str:
+            value_str = f"'{search_value}'"
+        elif type(search_value) == list:
+            value_str = str(search_value)
+        else:
+            raise ValueError(f"bad search value {search_value}")
+        # Build predicate
+        predicate = f"`{field_name[0]}`.`{search_field}` {operator} {value_str} "
+        return predicate
 
 def server_response(content, status_code):
     response = make_response(json.dumps(content))
