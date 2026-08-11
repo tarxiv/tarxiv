37058d36650c7e9b61c9deb2a6f11ac93c62b5cb
Jack Leland <jack.leland@dtc.ox.ac.uk>
2026-08-11 10:37:09 +0100

    ruff format + check

--- tarxiv/data_sources.py
+++ tarxiv/data_sources.py
@@ -897,9 +897,11 @@ class AlerceMod(TarxivModule):
 
         try:
             # Now get lsst
-            lsst_df = self.client.query_objects(ra=ra_deg, dec=dec_deg, radius=radius, survey="lsst")
+            lsst_df = self.client.query_objects(
+                ra=ra_deg, dec=dec_deg, radius=radius, survey="lsst"
+            )
             if "oid" in lsst_df.columns and not lsst_df["oid"].empty:
-                lsst_id = lsst_df["oid"].iat[0]
+                lsst_id = lsst_df["oid"].iloc[0]
                 meta["lsst_object_id"] = str(lsst_id)
 
                 # Get probabilities
